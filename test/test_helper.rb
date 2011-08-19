d5800458149f15f727c5c51b9de47f85797f2988
Jon Karna <ipg49vv2@gmail.com>
2011-08-19 15:17:55 -0500

    Moved models to test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,49 @@
+
 require 'test/unit'
 require 'turn'
 require 'shoulda'
+require 'active_record'
+require 'active_record/fixtures'
+require 'yaml'
+require 'sqlite3'
 require 'enumerated_field'
+
+ActiveRecord::Base.establish_connection :adapter => 'sqlite3', :database => ':memory:'
+ActiveRecord::Schema.define do
+  create_table :apples, :force => true do |t|
+    t.string :color
+    t.string :kind
+  end
+end
+
+class Apple < ActiveRecord::Base
+  include EnumeratedField
+
+  enum_field :color, [['Red', :red], ['Green', :green]], :validate => false
+  enum_field :kind, [['Fuji Apple', :fuji], ['Delicious Red Apple', :delicious]], :validate => false
+end
+
+class Banana
+  include EnumeratedField
+  include ActiveModel::Validations
+
+  attr_accessor :brand
+  attr_accessor :color
+  attr_accessor :tastiness
+
+  enum_field :brand, [["Chiquita", :chiquita], ["Del Monte", :delmonte]]
+  enum_field :color, [["Awesome Yellow", :yellow], ["Icky Green", :green]], :allow_nil => true
+  # stressing the constantizing of the keys
+  enum_field :tastiness, [
+    ["Great", "great!"],
+    ["Good", "it's good"],
+    ["Bad", "hate-hate"],
+  ], :validate => false
+
+  def initialize(brand, color)
+    self.brand = brand
+    self.color = color
+  end
+end
+
+Fixtures.create_fixtures 'test/fixtures', :apples
